27496175afe3fc06480f7e5f6acfb918013dffeb
ktj1997 <rlaxowns7916@gmail.com>
2022-06-23 23:31:53 +0900

    feat: vault-volume & Seal / Unseal

--- client/src/main/kotlin/example/spring/vault/config/datasource/DataSourceConfiguration.kt
+++ client/src/main/kotlin/example/spring/vault/config/datasource/DataSourceConfiguration.kt
@@ -2,10 +2,10 @@ package example.spring.vault.config.datasource
 
 import org.springframework.boot.jdbc.DataSourceBuilder
 import org.springframework.context.annotation.Bean
-import org.springframework.stereotype.Component
+import org.springframework.context.annotation.Configuration
 import javax.sql.DataSource
 
-@Component
+@Configuration
 class DataSourceConfiguration(
     private val dataSourceProperties : DataSourceProperties
 ) {
@@ -16,6 +16,6 @@ class DataSourceConfiguration(
             .username(dataSourceProperties.username)
             .password(dataSourceProperties.password)
             .driverClassName(dataSourceProperties.driverClassName)
-            .build();
+            .build()
     }
 }
